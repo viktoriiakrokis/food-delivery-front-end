a93d3c4bab1e67588123d8fefa49618bdb065af2
viktoriiakrokis <vika.krokis063@gmail.com>
2020-03-06 10:24:16 +0200

    moved show tags button to header

--- src/components/content/header/Header.js
+++ src/components/content/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import ListTags from './tags/ListTags'
 
 const HeaderContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     flex-wrap: wrap;
     width: 100%;
@@ -77,30 +78,76 @@ const SortControl = styled.select`
     font-size: 16px;
     cursor: pointer;
 `
+const ShowTags = styled.div``
+
+const Button = styled.button`
+    text-align: center;
+    height: 34px;
+    width: 180px;
+    background-color: #491C8A;
+    border-radius: 6px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #FFFFFF;
+    border: 1px solid #491C8A;
+    cursor: pointer;
+
+    :hover {
+        color: #491C8A;
+        border: 1px solid  #491C8A;
+        background-color: #FFFFFF;
+    }
+`
 
 const SortOption = styled.option``
 
-const Header = ({ restaurants, dispatchSort, dispatchSearch }) => (
-    <HeaderContainer>
-        <Title>Found <NumberOfRestaurants >{ restaurants.length} </NumberOfRestaurants>restaurants</Title>
-        <SearchBar>
-            <SearchRestaurant
-                id='searchbar'
-                type="text"
-                name="search"
-                placeholder="Search..."
-                onChange={ (event) => dispatchSearch(event.target.value)} />
-            <SearchLogo src="icon.png"/>
-        </SearchBar>
-        <SortContainer>
-            <SortLabel>Sort by:</SortLabel>
-            <SortControl id='sortbar' onChange={ (event) => dispatchSort(event.target.value) }>
-                <SortOption value='None'>None</SortOption>
-                <SortOption value='NameAsc'>Name A-Z</SortOption>
-                <SortOption value='NameDesc'>Name Z-A</SortOption>
-            </SortControl>
-        </SortContainer>
-    </HeaderContainer>
-)
+class Header extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            expanded: false  
+        }
+        this.toggleExpanded = this.toggleExpanded.bind(this);
+    }
+
+    toggleExpanded() {
+        this.setState({ expanded: !this.state.expanded })
+    }
+
+    render() {
+        const { restaurants, tags, selectedArrTags, dispatchSort, dispatchSearch, dispatchFilter } = this.props
+        return <HeaderContainer>
+            <Title>Found <NumberOfRestaurants >{ restaurants.length} </NumberOfRestaurants>restaurants</Title>
+            <SearchBar>
+                <SearchRestaurant
+                    id='searchbar'
+                    type="text"
+                    name="search"
+                    placeholder="Search..."
+                    onChange={ (event) => dispatchSearch(event.target.value) } />
+                <SearchLogo src="icon.png"/>
+            </SearchBar>
+            <SortContainer>
+                <SortLabel>Sort by:</SortLabel>
+                <SortControl id='sortbar' onChange={ (event) => dispatchSort(event.target.value) }>
+                    <SortOption value='None'>None</SortOption>
+                    <SortOption value='NameAsc'>Name A-Z</SortOption>
+                    <SortOption value='NameDesc'>Name Z-A</SortOption>
+                </SortControl>
+            </SortContainer>
+            <ShowTags>
+                <Button onClick={ this.toggleExpanded }>
+                <span>{ this.state.expanded ? 'Show tags' : 'Hide tags' }</span>
+                </Button>
+            </ShowTags>
+            { this.state.expanded && <ListTags
+                tags={ tags }
+                dispatchFilter={ dispatchFilter }
+                selectedArrTags={ selectedArrTags } />
+            }
+        </HeaderContainer>
+    }
+}  
+
 
 export default Header
---
+++ src/components/content/header/tags/ListTags.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import Tag from './Tag'
+
+const ListContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    @media (max-width: 340px) {
+        justify-content: center;
+    }
+`
+
+const ListTags = ({ tags, dispatchFilter, selectedArrTags }) => (
+    <ListContainer>
+        {tags.map(tag => 
+            <Tag dispatchFilter={ dispatchFilter } key={ tag } tag={ tag } isSelected={ selectedArrTags.includes(tag) }/>)
+        }
+    </ListContainer>
+)
+
+export default ListTags
